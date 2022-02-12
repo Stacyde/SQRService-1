2e017c53241f04370b52af431a0f5319324650a9
Stacy <79156969+Stacyde@users.noreply.github.com>
2022-02-12 21:45:48 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,7 +2,7 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int calculateSqrt(int a, int b) {
+    public int calculateNumberOfSquares(int a, int b) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
             if ((i * i >= a) & (i * i <= b)) {
